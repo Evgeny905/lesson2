b4e64d7aef2c4a7527c1a9e9bbb16f6d99010678
Evgeny905 <gorbatkovei@gmail.com>
2024-07-06 11:23:10 +0700

    lesson1 Изменил файл 2.4

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first = 1, second = 1):
+    if second != 0:
+        return first / second
+    else:
+        from math import inf
+        return inf
---
+++ module_4.1.py
@@ -0,0 +1,11 @@
+import true_math as tm
+import fake_math as fm
+
+result1 = tm.divide(69, 3)
+result2 = tm.divide(3, 0)
+result3 = fm.divide(49, 7)
+result4 = fm.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)

5c981cdebf6955cf78040357e8aca9860de19a86
Evgeny905 <gorbatkovei@gmail.com>
2024-07-21 15:29:46 +0700

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,45 @@
+class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        House.houses_history.append(args[0])
+        return cls.houses_history and super().__new__(cls)
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return f'Название: {self.name} Кол-во этажей: {self.number_of_floors}'
+    def __eq__(self, other):
+        return self.number_of_floors == other
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+    def __add__(self, value):
+        return House(self.name, self.number_of_floors + value)
+    def __radd__(self, value):
+        return House(self.name, self.number_of_floors + value)
+    def __iadd__(self, value):
+        return House(self.name, self.number_of_floors + value)
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)

607a72b0339d56e2f24ae69a8835bfa16643a164
Evgeny905 <gorbatkovei@gmail.com>
2024-07-24 16:55:26 +0700

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -7,22 +7,40 @@ class House:
     def __str__(self):
         return f'Название: {self.name} Кол-во этажей: {self.number_of_floors}'
     def __eq__(self, other):
+        print(isinstance(other, int))
+        print(isinstance(other, House))
         return self.number_of_floors == other
     def __lt__(self, other):
+        print(isinstance(other, int))
+        print(isinstance(other, House))
         return self.number_of_floors < other.number_of_floors
     def __le__(self, other):
+        print(isinstance(other, int))
+        print(isinstance(other, House))
         return self.number_of_floors <= other.number_of_floors
     def __gt__(self, other):
+        print(isinstance(other, int))
+        print(isinstance(other, House))
         return self.number_of_floors > other.number_of_floors
     def __ge__(self, other):
+        print(isinstance(other, int))
+        print(isinstance(other, House))
         return self.number_of_floors >= other.number_of_floors
     def __ne__(self, other):
+        print(isinstance(other, int))
+        print(isinstance(other, House))
         return self.number_of_floors != other.number_of_floors
     def __add__(self, value):
+        print(isinstance(value, int))
+        print(isinstance(value, House))
         return House(self.name, self.number_of_floors + value)
     def __radd__(self, value):
+        print(isinstance(value, int))
+        print(isinstance(value, House))
         return House(self.name, self.number_of_floors + value)
     def __iadd__(self, value):
+        print(isinstance(value, int))
+        print(isinstance(value, House))
         return House(self.name, self.number_of_floors + value)
 
 
